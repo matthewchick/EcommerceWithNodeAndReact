077a595400ed9a040157ed9ea9948ef978dfec05
Matthew Chick <c.chick@unsw.edu.au>
2019-09-19 12:22:14 +1000

    Add Product model

---
+++ models/product.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const {ObjectId} = mongoose.Schema;     //build relationship between product and category
+
+// define the schema
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 32
+        },
+        description: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 200
+        },
+        price: {
+            type: Number,
+            trim: true,
+            required: true,
+            maxlength: 32
+        },
+        price: {
+            type: ObjectId,
+            ref: 'Category',
+            required: true
+        },
+        quantity: {
+            type: Number,
+        },
+        photo: {
+            data: Buffer,
+            contentType: String
+        },
+        shipping: {
+            required: false,
+            type: Boolean
+        }
+    }, 
+    {timestamps: true}
+);
+
+//To use our schema definition, we need to convert our userSchema into a Model we can work with.
+//https://mongoosejs.com/docs/guide.html
+module.exports = mongoose.model("Product", productSchema);   //var User = mongoose.model('Category', categorySchema)
