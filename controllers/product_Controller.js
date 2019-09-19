27419172db4088e66d78c6254c61f6d92f86fb96
Matthew Chick <c.chick@unsw.edu.au>
2019-09-19 12:51:21 +1000

    how to call formidable and use fs package to handle file upload

--- controllers/product_Controller.js
+++ controllers/product_Controller.js
@@ -1,19 +1,37 @@
+const formidable = require('formidable');    //file upload
+const _ = require('lodash');                 //Lodash contains tools to simplify programming with strings, numbers, arrays, functions and objects.
+const fs = require('fs');
 const Product = require('../models/productModel');
 const {errorHandler} = require('../helpers/dbErrorHandler');
 
 exports.create = (req, res) => {
     console.log("req.body", req.body);
-    const category = new Category(req.body)
-    
-    category.save((err, data) => {
+    let form = new formidable.IncomingForm();    //
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
         if (err){
             return res.status(400).json({
-                error: errorHandler(err)
+                error: 'Image could not be uploaded'
             });
         }
-        
-        res.json({
-            data
+        let product = new Product(fields);
+
+        if (files.photo){
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType = files.photo.type
+        }
+
+        product.save((err, result)=> {
+            if (err){
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            
+            res.json({
+                result
+            });
         });
     });
+
 };
