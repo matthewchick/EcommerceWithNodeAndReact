8618e2870cd4e9b9e4cfa58ff463dca4458bb386
Matthew Chick <c.chick@unsw.edu.au>
2019-09-20 10:25:56 +1000

    Add delete and update product

--- controllers/product_Controller.js
+++ controllers/product_Controller.js
@@ -21,6 +21,70 @@ exports.getProduct = (req, res) => {
     return res.json(req.product);
 };
 
+exports.removeProduct = (req, res) => {
+    let product = req.product;
+    product.remove((err, deletedProduct) => {
+        if (err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            // show deleted product info
+            deletedProduct,
+            message: "Product deleted successfully"
+        });
+    });
+};
+
+exports.updateProduct = (req, res) => {
+    console.log("req.body", req.body);
+    let form = new formidable.IncomingForm();    //
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err){
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            });
+        }
+
+        // check for all fields
+        const {name, description, price, category, quantity, shipping} = fields
+
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+        let product = req.product;             //for update
+        product = _.extend(product, fields);
+
+        if (files.photo){
+            console.log("FILES PHOTO: ", files.photo);
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1mb in size"
+                });
+            }
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result)=> {
+            if (err){
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            
+            res.json({
+                result
+            });
+        });
+    });
+
+};
+
 exports.create = (req, res) => {
     console.log("req.body", req.body);
     let form = new formidable.IncomingForm();    //
--- routes/product_route.js
+++ routes/product_route.js
@@ -1,7 +1,7 @@
 // Create express router
 const express = require('express');
 const router = express.Router();
-const { create, productById, getProduct } = require('../controllers/product_Controller');   //destruct for functions
+const { create, productById, getProduct, removeProduct, updateProduct } = require('../controllers/product_Controller');   //destruct for functions
 const { userSignupValidator } = require('../validator/validator');
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth_Controller'); 
 const { userById } = require('../controllers/user_Controller');  
@@ -9,6 +9,8 @@ const { userById } = require('../controllers/user_Controller');
 // set the routes to the function inside the controller like php laravel or spring Boot getParam()
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);  //use userID to create product
 router.get("/product/:productId", getProduct);  //use productID to get the product
+router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, removeProduct);
+router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, updateProduct);
 
 router.param('userId', userById);
 router.param('productId', productById);
