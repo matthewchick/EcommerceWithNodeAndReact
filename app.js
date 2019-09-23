dfd0a7b713022740236ab4ab1da400e5553fc0a8
Matthew Chick <c.chick@unsw.edu.au>
2019-09-23 10:39:35 +1000

    read and update user profile and fix the warning for findbyIdandUpdate

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true, 
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 })
 .then(()=> console.log('DB Connected'));
 
--- routes/user_route.js
+++ routes/user_route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { userById } = require('../controllers/user_Controller');   //destruct for functions
+const { userById, readUser, updateUser } = require('../controllers/user_Controller');   //destruct for functions
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth_Controller'); 
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res)=> {
@@ -8,6 +8,10 @@ router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res)=> {
         user: req.profile
     });
 });
+
+router.get('/user/:userId', requireSignin, isAuth, readUser);
+router.put('/user/:userId', requireSignin, isAuth, updateUser);
+
 router.param('userId', userById);
 
 module.exports = router;   // allow the other file to import
